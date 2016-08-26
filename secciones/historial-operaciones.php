86573ff38ed5f63e88d7cf825ac26e89930233b5
Jorge Arreola <contacto@arrreola.mx>
2016-08-26 15:15:10 -0500

    ajustes de operaciones seccion

--- secciones/historial-operaciones.php
+++ secciones/historial-operaciones.php
@@ -2,6 +2,7 @@
 
 <ul class="op-ul">
     <?php
+    $infoOp = '';
     for ($op = 0; $op < $totOp; $op++):
 
         $qrySts = 'SELECT * FROM statuspro WHERE id_st=' . $rowOp[$op]['edo'];
@@ -11,7 +12,13 @@
 
         $nameOp = html_entity_decode($rowOp[$op]['tit_' . $len]);
         $shorOp = html_entity_decode($rowOp[$op]['desc_short_' . $len]);
-        $infoOp = html_entity_decode($rowOp[$op]['info_' . $len]);
+
+        if ($rowOp[$op]['info_' . $len] != '' && $rowOp[$op]['info_' . $len] != NULL && $rowOp[$op]['info_' . $len] != 'NULL'):
+            $infoOp = '<div class="op-statement">' . html_entity_decode($rowOp[$op]['info_' . $len]) . '</div>';
+        else:
+            $infoOp = '';
+        endif;
+
         $img = $rowOp[$op]['img'];
         $sts = $rowOp[$op]['status'];
         $btnInt = $rowOp[$op]['btn_' . $len];
@@ -21,10 +28,12 @@
         ?>
         <li class="op-li-elements">
             <div class="op-li-side-a">
+
                 <div class="op-li-header">
                     <span class="op-heading-project"><?= $nameOp; ?></span>
                     <span class="op-status-project"><?= $edoPro; ?></span>
                 </div>
+
                 <div class="op-li-content">
                     <?= $shorOp; ?>
                 </div>
@@ -34,12 +43,11 @@
                 </div>
 
             </div>
-            <div class="op-li-side-b">
-                <img class="op-img" src="img/operaciones/<?= $img; ?>" alt="">
-            </div>
-            <div class="op-statement">
-                <?= $infoOp; ?>
-            </div>
+            <div class="op-li-side-b"
+                 style="background:url('img/operaciones/<?= $img; ?>') no-repeat center / cover;"></div>
+
+            <?= $infoOp; ?>
+
         </li>
     <?php endfor; ?>
 </ul>
